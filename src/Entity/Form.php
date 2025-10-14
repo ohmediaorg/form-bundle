a9fc369edc510ed24b9808c1331ca08fb15c6a2f
Justin Hoffman <justin@ohmedia.ca>
2025-10-14 11:56:22 -0600

    entity fields

--- src/Entity/Form.php
+++ src/Entity/Form.php
@@ -2,12 +2,12 @@
 
 namespace OHMedia\FormBundle\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use OHMedia\FormBundle\Repository\FormRepository;
 use OHMedia\TimezoneBundle\Util\DateTimeUtil;
 use OHMedia\UtilityBundle\Entity\BlameableEntityTrait;
-
-// use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: FormRepository::class)]
 class Form
@@ -21,6 +21,18 @@ class Form
 
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $published_at = null;
+
+    #[ORM\Column(length: 50)]
+    #[Assert\NotBlank]
+    #[Assert\Length(max: 50)]
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::ARRAY)]
+    #[Assert\NotBlank]
+    private array $recipients = [];
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
 
     public function __toString(): string
     {
@@ -52,5 +64,41 @@ class Form
     public function isScheduled(): bool
     {
         return $this->published_at && DateTimeUtil::isFuture($this->published_at);
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getRecipients(): array
+    {
+        return $this->recipients;
+    }
+
+    public function setRecipients(?array $recipients): static
+    {
+        $this->recipients = $recipients;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
