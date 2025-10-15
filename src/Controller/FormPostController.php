7fcf3b710578494b2b4538fdb504ae7200c627a7
Justin Hoffman <justin@ohmedia.ca>
2025-10-15 16:41:41 -0600

    internal vs external email

--- src/Controller/FormPostController.php
+++ src/Controller/FormPostController.php
@@ -10,6 +10,7 @@ use OHMedia\FormBundle\Service\FormBuilder;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -58,68 +59,107 @@ class FormPostController extends AbstractController
             ]);
         }
 
-        $recipients = $formEntity->getRecipients();
-
         try {
-            $to = [];
-
-            foreach ($recipients as $recipient) {
-                $to[] = new EmailAddress($recipient);
-            }
-
-            $replyTo = [];
-            $copy = [];
-
-            $formData = [];
-
-            foreach ($formEntity->getFields() as $field) {
-                $name = $field->getName();
-                $label = $field->getLabel();
-                $value = $form->get($name)->getData();
-
-                if ($value instanceof \DateTimeInterface) {
-                    $formData[$label] = $value->format('M j, Y');
-                } elseif (is_array($value)) {
-                    $formData[$label] = htmlspecialchars(implode(', ', $value));
-                } elseif ($field->isTypeTextarea()) {
-                    $formData[$label] = nl2br(htmlspecialchars($value));
-                } else {
-                    $formData[$label] = htmlspecialchars($value);
-                }
-
-                if ($field->isTypeEmail()) {
-                    $fieldData = $field->getData();
-
-                    if ($fieldData['copy']) {
-                        $copy[] = new EmailAddress($value);
-                    }
-
-                    if ($fieldData['reply']) {
-                        $replyTo[] = new EmailAddress($value);
-                    }
-                }
-            }
-
-            // TODO: internal vs external emails
-
-            $subject = $formEntity->getSubject();
-
-            $email = (new Email())
-                ->setSubject($subject)
-                ->setTemplate('@OHMediaForm/email/form_email.html.twig', [
-                    'form_entity' => $formEntity,
-                    'form_data' => $formData,
-                    'subject' => $subject,
-                ])
-                ->setTo(...$to)
-                ->setReplyTo(...$replyTo)
-            ;
-
-            $emailRepository->save($email, true);
+            $this->sendEmails($emailRepository, $formEntity, $form);
         } catch (\Exception $e) {
             return new JsonResponse($e->getMessage(), 500);
         }
 
         return new JsonResponse(['success' => true]);
+    }
+
+    private function sendEmails(
+        EmailRepository $emailRepository,
+        Form $formEntity,
+        FormInterface $form,
+    ): void {
+        $to = [];
+
+        foreach ($formEntity->getRecipients() as $recipient) {
+            $to[] = new EmailAddress($recipient);
+        }
+
+        list($replyTo, $copy) = $this->getReplyToAndCopyEmails(
+            $formEntity,
+            $form,
+        );
+
+        $formData = [];
+
+        foreach ($formEntity->getFields() as $field) {
+            $label = $field->getLabel();
+            $value = $form->get($field->getName())->getData();
+
+            if ($value instanceof \DateTimeInterface) {
+                $formData[$label] = $value->format('M j, Y');
+            } elseif (is_array($value)) {
+                $formData[$label] = htmlspecialchars(implode(', ', $value));
+            } elseif ($field->isTypeTextarea()) {
+                $formData[$label] = nl2br(htmlspecialchars($value));
+            } else {
+                $formData[$label] = htmlspecialchars($value);
+            }
+        }
+
+        $subject = $formEntity->getSubject();
+
+        $email = (new Email())
+            ->setSubject($subject)
+            ->setTemplate('@OHMediaForm/email/form_internal.html.twig', [
+                'form_entity' => $formEntity,
+                'form_data' => $formData,
+                'subject' => $subject,
+            ])
+            ->setTo(...$to)
+            ->setReplyTo(...$replyTo)
+        ;
+
+        $emailRepository->save($email, true);
+
+        if ($copy) {
+            $email = (new Email())
+                ->setSubject($subject)
+                ->setTemplate('@OHMediaForm/email/form_external.html.twig', [
+                    'form_entity' => $formEntity,
+                    'form_data' => $formData,
+                    'subject' => $subject,
+                ])
+                ->setTo(...$copy)
+            ;
+
+            $emailRepository->save($email, true);
+        }
+    }
+
+    private function getReplyToAndCopyEmails(
+        Form $formEntity,
+        FormInterface $form,
+    ): array {
+        $replyTo = [];
+        $copy = [];
+
+        foreach ($formEntity->getFields() as $field) {
+            if (!$field->isTypeEmail()) {
+                continue;
+            }
+
+            $value = $form->get($field->getName())->getData();
+
+            if (!$value) {
+                continue;
+            }
+
+            $fieldData = $field->getData();
+
+            if ($fieldData['reply']) {
+                $replyTo[] = new EmailAddress($value);
+            }
+
+            if ($fieldData['copy']) {
+                $copy[] = new EmailAddress($value);
+            }
+        }
+
+        return [$replyTo, $copy];
     }
 }
